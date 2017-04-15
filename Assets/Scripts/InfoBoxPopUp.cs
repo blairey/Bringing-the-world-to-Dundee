d7d45273f47fcad661d0c67e3e0b77f296b4e2c3
blairey <blaireemilymcleod@gmail.com>
2017-04-15 13:36:17 +0100

    sphere 1 with infobox
    photosphere working
    infobox with working trigger

---
+++ Assets/Scripts/InfoBoxPopUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class InfoBoxPopUp : MonoBehaviour
+{
+    public float gazeTimer = 2f;
+
+    private float timer;
+
+    private bool gazeAtObj;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gazeAtObj == true)
+        {
+            timer += Time.deltaTime;
+            
+            if(timer >-gazeTimer)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+
+    }
+
+    public void PointerEnter()
+    {
+        gazeAtObj = true;
+    }
+ 
+    public void PointerExit()
+    {
+        gazeAtObj = false;
+    }
+
+    public void PointerDown()
+    {
+        Debug.Log(" down ");
+    }
+
+}
+
